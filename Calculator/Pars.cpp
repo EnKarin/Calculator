04cb8704364344c24a449de5774cdbdaaee247ba
Алексей Кожемякин <aleksey.k2000@mail.ru>
2019-05-05 00:25:59 +0700

    Streamlined function parse.

--- Calculator/Pars.cpp
+++ Calculator/Pars.cpp
@@ -72,120 +72,106 @@ string parse(string s)
 	int i, j;
 	int delta;//для того, чтобы смещать курсор текущего знака после изменения строки
 	bool forsvitch; // чтобы разделять плюс и минус; умножение и деление внути одного цикла
-	bool close = false; //условие завершения цикла
-	while (!close)
+	for (int n = 0; n < s.size(); n++)//корень
 	{
-		for (int n = 0; n < s.size(); n++)//корень
+		if (s[n] == 'S')
 		{
-			if (s[n] == 'S')
+			n += 3;//проскакиваем надпись Sqrt
+			if (s[n + 1] == '(')//правая часть от плюса
 			{
-				n += 3;//проскакиваем надпись Sqrt
-				if (s[n + 1] == '(')//правая часть от плюса
+				second = brackri(s, n + 1, delta);//получаем результат в виде строки 
+				i = n + delta + 1;//узнаём её длину для replace 
+			}
+			else
+				second = getsecnum(s, n, i);
+			if (second < 0)
+			{
+				s = "There is no negative root";
+				break;
+			}
+			else
+			n = replace(s, n - 3, i - 1, Root(second));//курсор на последний символ результата операции
+		}
+	}
+
+	for (int n = 1; n < s.size(); n++)//степень
+	{
+		if (s[n] == '^')
+		{
+			if (s[n - 1] == ')')//левая часть от знака
+			{
+				first = bracklf(s, n - 1, delta);
+				j = n - delta - 1;
+			}
+			else
+				first = getfirnum(s, n, j);
+
+			n = replace(s, j + 1, n, Expon(first));//курсор на последний символ результата операции
+		}
+	}
+
+	for (int n = 1; n < s.size(); n++)//умножение и деление
+	{
+		if (s[n] == '*' || s[n] == '/')
+		{
+			forsvitch = (s[n] == '*' ? true : false);
+			if (s[n + 1] == '(')//правая часть от плюса
+			{
+				second = brackri(s, n + 1, delta);//получаем результат в виде строки 
+				i = n + delta + 1;//узнаём её длину для replace 
+			}
+			else
+				second = getsecnum(s, n, i);
+
+			if (s[n - 1] == ')')//левая часть от знака
+			{
+				first = bracklf(s, n - 1, delta);
+				j = n - delta - 1;
+			}
+			else
+				first = getfirnum(s, n, j);
+			if (forsvitch)
+				n = replace(s, j + 1, i - 1, Multi(first, second));//курсор на последний символ результата операции
+			else
+			{
+				if (second == 0)
 				{
-					second = brackri(s, n + 1, delta);//получаем результат в виде строки 
-					i = n + delta + 1;//узнаём её длину для replace 
-				}
-				else
-					second = getsecnum(s, n, i);
-				if (second < 0)
-				{
-					s = "There is no negative root";
-					goto End;
+					s = "Division by zero is impossible";
 					break;
 				}
 				else
-				n = replace(s, n - 3, i - 1, Root(second));//курсор на последний символ результата операции
-			}
-		}
-
-		for (int n = 1; n < s.size(); n++)//степень
-		{
-			if (s[n] == '^')
-			{
-				if (s[n - 1] == ')')//левая часть от знака
-				{
-					first = bracklf(s, n - 1, delta);
-					j = n - delta - 1;
-				}
-				else
-					first = getfirnum(s, n, j);
-
-				n = replace(s, j + 1, n, Expon(first));//курсор на последний символ результата операции
-			}
-		}
-
-		for (int n = 1; n < s.size(); n++)//умножение и деление
-		{
-			if (s[n] == '*' || s[n] == '/')
-			{
-				forsvitch = (s[n] == '*' ? true : false);
-				if (s[n + 1] == '(')//правая часть от плюса
-				{
-					second = brackri(s, n + 1, delta);//получаем результат в виде строки 
-					i = n + delta + 1;//узнаём её длину для replace 
-				}
-				else
-					second = getsecnum(s, n, i);
-
-				if (s[n - 1] == ')')//левая часть от знака
-				{
-					first = bracklf(s, n - 1, delta);
-					j = n - delta - 1;
-				}
-				else
-					first = getfirnum(s, n, j);
-				if (forsvitch)
-					n = replace(s, j + 1, i - 1, Multi(first, second));//курсор на последний символ результата операции
-				else
-				{
-					if (second == 0)
-					{
-						s = "Division by zero is impossible";
-						goto End;
-						break;
-					}
-					else
 					n = replace(s, j + 1, i - 1, Div(first, second));
-				}
-
 			}
+
 		}
-
-		for (int n = 1; n < s.size(); n++)//сложение и вычитание
-		{
-			if (s[n] == '+' || s[n] == '-')
-			{
-				forsvitch = (s[n] == '+' ? true : false);
-				if (s[n + 1] == '(')//правая часть от плюса
-				{
-					second = brackri(s, n + 1, delta);//получаем результат в виде строки 
-					i = n + delta + 1;//узнаём её длину для replace 
-				}
-				else
-					second = getsecnum(s, n, i);
-
-				if (s[n - 1] == ')')//левая часть от знака
-				{
-					first = bracklf(s, n - 1, delta);
-					j = n - delta - 1;
-				}
-				else
-					first = getfirnum(s, n, j);
-
-				if (forsvitch)
-					n = replace(s, j + 1, i - 1, Sum(first, second));
-				else
-					n = replace(s, j + 1, i - 1, Diff(first, second));
-			}
-		}
-		close = true;
-		for (int k = 0; k < s.size(); k++)//завершаем цикл обработки, когда в строке останутся только цифры или допустимые символы
-			if (!(isdigit(s[k]) || s[k] == '.' || s[k] == '-'))
-			{
-				close = false;
-				break;
-			}
 	}
-	End:
+
+	for (int n = 1; n < s.size(); n++)//сложение и вычитание
+	{
+		if (s[n] == '+' || s[n] == '-')
+		{
+			forsvitch = (s[n] == '+' ? true : false);
+			if (s[n + 1] == '(')//правая часть от плюса
+			{
+				second = brackri(s, n + 1, delta);//получаем результат в виде строки 
+				i = n + delta + 1;//узнаём её длину для replace 
+			}
+			else
+				second = getsecnum(s, n, i);
+
+			if (s[n - 1] == ')')//левая часть от знака
+			{
+				first = bracklf(s, n - 1, delta);
+				j = n - delta - 1;
+			}
+			else
+				first = getfirnum(s, n, j);
+
+			if (forsvitch)
+				n = replace(s, j + 1, i - 1, Sum(first, second));
+			else
+				n = replace(s, j + 1, i - 1, Diff(first, second));
+		}
+	}
 	return s;
 }
