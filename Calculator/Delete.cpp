05f008f16176c77145a1b4ac71419ee96687d7a7
Карина Елагина <karina.elagina2013@yandex.ru>
2019-05-15 19:23:32 +0700

    Exceptions worked for backspace

--- Calculator/Delete.cpp
+++ Calculator/Delete.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 string Delete(string s)
 {
-	s.erase(s.size() - 1, 1);
+	if (s != "0")
+	    s.erase(s.size() - 1, 1);
+	if (s == "0" || s.size() == 0)
+		s = "0";
 	return s;
 }
