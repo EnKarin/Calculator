2ee1f0f50cd39aa9b40092f69dcd0f2e1a061091
Карина Елагина <karina.elagina2013@yandex.ru>
2019-05-30 21:06:39 +0700

    Fixed a bug with a minus at the beginning of the line before the brackets

--- Calculator/Check.cpp
+++ Calculator/Check.cpp
@@ -15,20 +15,7 @@ string check(string s)
 			k--;
 	}
 	if (k == 0)
-	{
-		s = parse(s);
-		for (int i = 0; i < s.size();)
-		{
-			if (s[i] == '(' || s[i] == ')')
-				s.erase(i, 1);
-			else
-				i++;
-		}
-		for (int i = 0; i < s.size() - 1; i++)
-			if (s.substr(i, 2) == "--")
-				s.erase(i, 2);
-		return s;
-	}
+		return parse(s);
 	else
 		return "Incorrect brackets";
 }
